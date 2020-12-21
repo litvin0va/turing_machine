db126b6fdb8603da8aee3f91355e79cb2fb98bb1
Elizaveta <elizaveta@localhost.localdomain>
2020-12-21 18:12:14 +0300

    Revert "test of binary comparison"
    This reverts commit 61723b8173c677d3e2a65d19f7268c1f805c209d.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-PRINT_STEPS = False
-STEP_BY_STEP = False
+PRINT_STEPS = True
+STEP_BY_STEP = True
 
 class Action:
     def __init__(self, current_symbol, new_symbol, direction, new_state):
@@ -55,9 +55,9 @@ def print_tape(tape):
         current_tape += tape[key]
     print(current_tape)
 
-
 def pharse_file(filename):
     file = open(filename, "r")
+    input_tape = file.readline()
     mashine = Turing()
 
     for line in file:
@@ -70,104 +70,75 @@ def pharse_file(filename):
         current_state = mashine.get_state(current_state_index)
         action = Action(words_list[1], words_list[2], words_list[3], words_list[4])
         current_state.actions.append(action)
-    return mashine
+    return input_tape, mashine
 
 
 
+input_tape, mashine = pharse_file("input.txt")
+input_tape = input_tape.rstrip('\n') #to remove last symbol
+input_tape = input_tape.replace(" ", "_")
+print(input_tape)
 
-for input_tape in ["1 1",
-                   "10 10",
-                   "1 10",
-                   "10 1",
-                   "100 100",
-                   "1011 1011",
-                   "11 101",
-                   "101 11",
-                   "110 10",
-                   "100000 1",
-                   "100 100000",
-                   "11101010 100101010101001",
-                   "10111 10000",
-                   "1001 101",
-                   "101111111111111 1011",
-                   "111111 101111111111111111111",
-                   "10100011010001 10100011010001",
-                   "1 100000000000000000000",
-                   "100000000000000000000 1",
-                   "101 100",
-                   "100 101",
-                   "1 0",
-                   "0 1",
-                   ]:
-    print("\n")
-    print(input_tape)
-    mashine = pharse_file("input.txt")
-    input_tape = input_tape.replace(" ", "_")
+tape = {}
+for i in range(len(input_tape)):
+    tape[i] = input_tape[i]
 
-    tape = {}
-    for i in range(len(input_tape)):
-        tape[i] = input_tape[i]
+head = 0
+current_state = mashine.get_state(mashine.get_state_index_by_name("0"))
+step = 0
 
-    head = 0
-    current_state = mashine.get_state(mashine.get_state_index_by_name("0"))
-    step = 0
+while True:
+    step += 1
+    try:
+        current_tape_symbol = tape[head]
+    except:
+        tape[head] = "_"
+        current_tape_symbol = None
 
-    while True:
-        step += 1
-        try:
-            current_tape_symbol = tape[head]
-        except:
-            tape[head] = "_"
-            current_tape_symbol = None
+    if STEP_BY_STEP:
+        command = input()  # q = Quit , s = Step, i = Info
 
-        if STEP_BY_STEP:
+        while command == "i":
+            print("----------------------------------------------")
+            print("Current State:", current_state.name)
+            current_tape = ""
+            head_tape = ""
+            for key in sorted(tape.keys()):
+                current_tape += tape[key]
+                if key == head:
+                    head_tape += "+"
+                else:
+                    head_tape += "_"
+            print(current_tape)
+            print(head_tape)
+
+            print("----------------------------------------------")
             command = input()  # q = Quit , s = Step, i = Info
 
-            while command == "i":
-                print("----------------------------------------------")
-                print("Current State:", current_state.name)
-                current_tape = ""
-                head_tape = ""
-                for key in sorted(tape.keys()):
-                    current_tape += tape[key]
-                    if key == head:
-                        head_tape += "+"
-                    else:
-                        head_tape += "_"
-                print(current_tape)
-                print(head_tape)
-
-                print("----------------------------------------------")
-                command = input()  # q = Quit , s = Step, i = Info
-
-            if command == "q":
-                break
-            elif command != "s":
-                print("Wrong command!")
-                break
-
-        action = current_state.find_action(current_tape_symbol)
-        if action.new_symbol != '*':
-            tape[head] = action.new_symbol
-
-        if action.direction == "l":
-            head -= 1
-        elif action.direction == "r":
-            head += 1
-        elif action.direction != "*":
-            raise Exception("Error! Incorrect Direction!")
-
-        if PRINT_STEPS == True:
-            print_tape(tape)
-
-        if action.new_state == "halt" or action.new_state == "halt-accept" or action.new_state == "halt-reject":
-            current_tape = ""
-            for key in sorted(tape.keys()):
-                if tape[key] != "_":
-                    current_tape += tape[key]
-            print(current_tape)
+        if command == "q":
             break
-        else:
-            current_state = mashine.get_state(mashine.get_state_index_by_name(action.new_state))
+        elif command != "s":
+            print("Wrong command!")
+            break
 
-    #print("Total Steps:", step)
+
+    action = current_state.find_action(current_tape_symbol)
+    if action.new_symbol != '*':
+        tape[head] = action.new_symbol
+
+    if action.direction == "l":
+        head -= 1
+    elif action.direction == "r":
+        head += 1
+    elif action.direction != "*":
+        raise Exception("Error! Incorrect Direction!")
+
+    if PRINT_STEPS == True:
+        print_tape(tape)
+
+    if action.new_state == "halt" or action.new_state == "halt-accept" or action.new_state == "halt-reject":
+        print(action.new_state)
+        break
+    else:
+        current_state = mashine.get_state(mashine.get_state_index_by_name(action.new_state))
+#print("Total Steps:", step)
