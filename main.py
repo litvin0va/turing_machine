8110ac6db4c55a9793902462c4cff9d4cbade7e9
Elizaveta <elizaveta@localhost.localdomain>
2020-12-21 18:05:47 +0300

    debug version

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 PRINT_STEPS = True
+STEP_BY_STEP = True
 
 class Action:
     def __init__(self, current_symbol, new_symbol, direction, new_state):
@@ -48,7 +49,11 @@ class Turing:
     def get_state(self, index):
         return self.states[index]
 
-
+def print_tape(tape):
+    current_tape = ""
+    for key in sorted(tape.keys()):
+        current_tape += tape[key]
+    print(current_tape)
 
 def pharse_file(filename):
     file = open(filename, "r")
@@ -87,7 +92,36 @@ while True:
     try:
         current_tape_symbol = tape[head]
     except:
+        tape[head] = "_"
         current_tape_symbol = None
+
+    if STEP_BY_STEP:
+        command = input()  # q = Quit , s = Step, i = Info
+
+        while command == "i":
+            print("----------------------------------------------")
+            print("Current State:", current_state.name)
+            current_tape = ""
+            head_tape = ""
+            for key in sorted(tape.keys()):
+                current_tape += tape[key]
+                if key == head:
+                    head_tape += "+"
+                else:
+                    head_tape += "_"
+            print(current_tape)
+            print(head_tape)
+
+            print("----------------------------------------------")
+            command = input()  # q = Quit , s = Step, i = Info
+
+        if command == "q":
+            break
+        elif command != "s":
+            print("Wrong command!")
+            break
+
+
     action = current_state.find_action(current_tape_symbol)
     if action.new_symbol != '*':
         tape[head] = action.new_symbol
@@ -100,15 +134,11 @@ while True:
         raise Exception("Error! Incorrect Direction!")
 
     if PRINT_STEPS == True:
-        current_tape = ""
-        for key in sorted(tape.keys()):
-            current_tape += tape[key]
-        print(current_tape)
+        print_tape(tape)
 
     if action.new_state == "halt" or action.new_state == "halt-accept" or action.new_state == "halt-reject":
         print(action.new_state)
         break
     else:
         current_state = mashine.get_state(mashine.get_state_index_by_name(action.new_state))
-
 #print("Total Steps:", step)
